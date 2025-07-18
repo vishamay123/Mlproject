a494a47cdfbc48f0c7e5ef196da8b27cfa4b82ad
vishamay123 <vishamay501@gmail.com>
2025-07-19 01:13:22 +0530

    Update app.py by vishamay

--- app.py
+++ app.py
@@ -8,14 +8,11 @@ application = Flask(__name__)
 
 app = application
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 
 
-@app.route('/predict', methods=['POST','GET'])
+@app.route('/', methods=['POST','GET'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
